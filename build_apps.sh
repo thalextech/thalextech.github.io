242fa477f4ef81e2c41962cd5d893e7194902481
Greg Netsman <greg.netsman@thalex.com>
2026-01-27 20:29:28 +0100

    build: exit on error

--- build_apps.sh
+++ build_apps.sh
@@ -1,3 +1,5 @@
+set -e
+
 REPO_ROOT="$(pwd)"
 
 APPS_ROOT="${REPO_ROOT}/apps"
@@ -6,7 +8,7 @@ APPS_DEPLOY_DIR="${REPO_ROOT}/apps_deployment"
 rm -rf "${APPS_DEPLOY_DIR}"
 mkdir -p "${APPS_DEPLOY_DIR}"
 
-for app in $(ls "${APPS_ROOT}"); do
+for app in $(find "${APPS_ROOT}" -type d -depth 1); do
     cd apps/"$app"
     npm ci
     VITE_BASE_PATH=/$app/ npm run build
