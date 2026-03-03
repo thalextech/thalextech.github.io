b2ccc49122ed7f906b38502cc0cf8d3f0c2a9244
hendrik <hendrik@thalex.com>
2026-03-03 17:24:50 +0100

    Removed broken apps

--- apps/volatility-backtest/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "../../../lib/styles.css";
-import "./styles.css";
-
-const el = document.getElementById("app");
-if (el && !el.querySelector(".app")) {
-  createApp(App).mount("#app");
-}
